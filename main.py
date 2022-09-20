e704e871087cb778905caa88fa3f0297f9bf172d
Neil Sarkar <neil@debian-BULLSEYE-live-builder-AMD64>
2022-09-20 00:21:59 -0700

    ocr working

---
+++ main.py
@@ -0,0 +1,19 @@
+def detect_text(path):    
+    """Detects text in the file."""    
+    from google.cloud import vision    
+    import io    
+    client = vision.ImageAnnotatorClient()    
+    with io.open(path, 'rb') as image_file:        
+    	content = image_file.read()    
+    image = vision.Image(content=content)    
+    
+    response = client.text_detection(image=image)    
+    texts = response.text_annotations   
+    
+    print('Texts:')    
+    for text in texts:        
+        print('\n"{}"'.format(text.description))        
+        vertices = (['({},{})'.format(vertex.x, vertex.y)                    
+                    for vertex in text.bounding_poly.vertices])        
+        print('bounds: {}'.format(','.join(vertices)))
+detect_text("data/test2.jpg")
