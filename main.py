382c5de71a3eb228ef379609ea640dd0a148b71c
Neil Sarkar <neil@debian-BULLSEYE-live-builder-AMD64>
2022-09-22 00:11:16 -0700

    set up webserver and started working on home page

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-def detect_text(path):    
-    """Detects text in the file."""    
-    from google.cloud import vision    
-    import io    
+import requests, json, random, time, threading
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+from google.cloud import vision    
+import io
+app = Flask(__name__, template_folder='frontend')
+def detect_text(path):   
+          
     client = vision.ImageAnnotatorClient()    
-    with io.open(path, 'rb') as image_file:        
-    	content = image_file.read()    
+    with io.open(path, 'rb') as image_file:
+      content = image_file.read()    
     image = vision.Image(content=content)    
     
     response = client.text_detection(image=image)    
@@ -16,4 +20,11 @@ def detect_text(path):
         vertices = (['({},{})'.format(vertex.x, vertex.y)                    
                     for vertex in text.bounding_poly.vertices])        
         print('bounds: {}'.format(','.join(vertices)))
-detect_text("data/test2.jpg")
+#detect_text("data/test2.jpg")
+
+@app.route('/')
+def homepage():
+  return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
