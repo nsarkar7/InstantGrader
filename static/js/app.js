bea0438e80a80eab72726a7e1de3efe3dd5b421a
Neil Sarkar <neil@debian-BULLSEYE-live-builder-AMD64>
2022-10-12 00:16:12 -0700

    working on assignment addition

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,6 @@
+var classes_list = {};
+var questions_added = 0;
+
 function login_check() {
     if(sessionStorage.getItem("name") === null || sessionStorage.getItem("email") === null || sessionStorage.getItem("picture") === null){
         window.location.replace("/")
@@ -13,8 +16,41 @@ function display_user_info() {
     let picture = sessionStorage.getItem("picture");
     document.getElementById("profile_button_text").innerHTML = name;
     document.getElementById("profile_button_img").src = picture;
+}
 
+function display_assignments(class_name) {
 
+    let selected_class = null;
+    let assignments = null;
+
+    for(let i=0; i<classes_list.length; i++) {
+        let individual_class = classes_list[i];
+        if(class_name == individual_class.class_name){
+            selected_class = individual_class;
+            assignments = selected_class.assignments;
+            console.log("sus");
+            break;
+        }
+    }
+    if(assignments.length == 0) {
+        if(document.getElementById("no_assignments_text") == undefined){
+            let main = document.getElementById("main");
+            let no_assignments_text = document.createElement("h3")
+            no_assignments_text.innerHTML = "Looks like you have not assigned anything yet. Use the button above to create a new one."
+            no_assignments_text.className = "no_assignments_text";
+            no_assignments_text.id = "no_assignments_text";
+            main.appendChild(no_assignments_text);
+        }
+    }
+    for(let i=0; i<assignments.length; i++) {
+        let main = document.getElementById("main");
+        let assignment_btn = document.createElement("button");
+        assignment_btn.innerHTML = individual_class.class_name;
+        assignment_btn.className = "assignment_button";
+        sidebar.appendChild(class_btn);
+    }
+    
+    
 }
 
 function get_classes() {
@@ -25,12 +61,14 @@ function get_classes() {
     get_classes_request.onreadystatechange = function() {
         if (this.status == 200 && this.readyState == 4) {
             let classes = JSON.parse(this.responseText);
+            classes_list = classes;
             for(let i=0; i<classes.length; i++){
                 let individual_class = classes[i];
                 let sidebar = document.getElementById("sidebar");
                 let class_btn = document.createElement("button");
                 class_btn.innerHTML = individual_class.class_name;
                 class_btn.className = "class_button";
+                class_btn.onclick = () => display_assignments(individual_class.class_name);
                 sidebar.appendChild(class_btn);
             }
         }
